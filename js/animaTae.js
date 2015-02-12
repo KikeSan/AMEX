d9983f5f520c1970d637b52f16213b2610d95b68
Enrique Sanchez <kikin1601@hotmail.com>
2015-02-12 16:00:25 -0500

    Web con animaciones

---
+++ js/animaTae.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    TweenMax.from(".cabecera", 0.5, {opacity:0,ease: Power1.easeOut});
+    TweenMax.from(".item", 0.5, {opacity:0,ease: Power1.easeOut});
+	
+    $('#boton1').click(function(){
+                TweenMax.to(".cabecera", 0.5, {opacity:0,ease: Power1.easeOut});
+                TweenMax.to(".item", 0.5, {opacity:0,ease: Power1.easeOut});
+                TweenMax.delayedCall(0.5, myFunction);				
+			})
+    
+    $('#boton1').hover(function(){
+				$(this).css('color','#EBEBEB')
+				$(this).css('background-color','#858585')
+			}, function(){
+				$(this).css('color','#858585')
+				$(this).css('background-color','#EBEBEB')
+			})
+    
+
+    function myFunction() {
+        window.location.href = 'tae.html';
+    }
+
+})
